e3e704b08ed44245b5d0e58cbc5249961a8d3492
gelifatsy <elias.assamnew@gmail.com>
2024-02-10 17:01:10 +0300

    flutter App: map integration

---
+++ mobile_app_flutter/lib/ethereum_integration.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_web3/ethereum.dart';
+
+class MetaMaskProvider extends ChangeNotifier {
+  static const connectingField = 0;
+  String currentAddress = '';
+  var account = "";
+  int? currentChain;
+  bool get isEnabled => ethereum != null;
+  bool get isInOperatingChain => currentChain == connectingField;
+  bool get isConnected => isEnabled && currentAddress.isNotEmpty;
+  Future connect() async {
+    if (isEnabled) {
+      final accs = await ethereum!.requestAccount();
+      account = accs[0];
+      if (accs.isNotEmpty) currentAddress = accs.first;
+      currentChain = await ethereum!.getChainId();
+      notifyListeners();
+    }
+  }
+}
